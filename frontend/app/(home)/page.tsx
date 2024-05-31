9db6d1839b370bc1e0c99f4477d09852b14c9ec4
yuvraj-singh-lodhi <it2005@global.org.in>
2024-05-31 15:16:28 +0530

    ui change

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
           <Navbar isFooter={false} />
           <HeroSection />
         </div>
-      </div>
       <div className="max-w-7xl mx-auto p-5">
         <Monuments />
         <Blog />
         <div className="border-t mt-10">
           <Navbar isFooter={true} className="flex-col gap-4" />
         </div>
+      </div>
       </div>
     </div>
   );
